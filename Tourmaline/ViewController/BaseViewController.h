012911e58f7999b4fcd892d0a29003198623aa1c
previz <previz@previzdeMBP.lan>
2016-06-06 13:26:55 +0800

    add baseControlller

---
+++ Tourmaline/ViewController/BaseViewController.h
@@ -0,0 +1,31 @@
+//
+//  BaseViewController.h
+//  Tourmaline
+//
+//  Created by previz on 16/6/6.
+//  Copyright © 2016年 dongan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+/**
+ *   navigation left right button
+ */
+@property (strong, nonatomic) UIButton      *leftNavigationBtn;
+@property (strong, nonatomic) UIButton      *rightNavigationBtn;
+
+/**
+ *  @param leftNavigationBtn
+ *  导航栏左侧button的点击事件
+ */
+- (void)leftNavigationBtnClick:(UIButton *)leftNavigationBtn;
+
+/**
+ *  @param rightNavigationBtn
+ *  导航栏右侧button的点击事件
+ */
+- (void)rightNavigationBtnClick:(UIButton *)rightNavigationBtn;
+
+@end
